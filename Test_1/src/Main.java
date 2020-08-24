361e9a951c3ee8ddd3744a37a7ff86d0ee61e930
Roboplayer999 <Christian.glaeser05@gmail.com>
2020-08-24 19:10:38 +0200

    Main

--- Test_1/src/Main.java
+++ Test_1/src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
+    public static int Bread;
     public static int test;
+    public static void main(String[] args) {
+        test = 5;
+        Bread = 50;
+        System.out.println("Hi " + test);
+        Test(20);
+        Bread = 50;
+    }
+
+    public static void Test (int Bread) {
+        System.out.println("Test " + Bread);
+    }
 }
